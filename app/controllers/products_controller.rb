8a5053e2fe2c954e26b907f242707362b49dad6a
Getro Naissance <getro89@gmail.com>
2017-02-10 09:42:04 -0800

    it will pass five rspec tests germane to the product model

---
+++ app/controllers/products_controller.rb
@@ -0,0 +1,43 @@
+class ProductsController < ApplicationController
+  def index
+    @products = Product.all
+  end
+
+  def show
+    @product = Product.find(params[:id])
+  end
+
+  def new
+    @product = Product.new
+  end
+
+  def edit
+    @product = Product.find(params[:id])
+  end
+
+  def create
+    @product = Product.new(product_params)
+    if @product.save
+      redirect_to products_path
+    else
+      render :new
+    end
+  end
+
+  def update
+    @product = Product.find(params[:id])
+
+
+  end
+
+  def destroy
+    @product = Product.find(params[:id])
+    @product.destroy
+    redirect_to products_path
+  end
+
+  private
+    def product_params
+      params.require(:products).permit(:name, :cost, :origin)
+    end
+end
--- spec/models/product_spec.rb
+++ spec/models/product_spec.rb
@@ -3,3 +3,7 @@ require 'rails_helper'
 RSpec.describe Product, type: :model do
   pending "add some examples to (or delete) #{__FILE__}"
 end
+
+describe Product do
+  it { should validate_presence_of :name }
+end
